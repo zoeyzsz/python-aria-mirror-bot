2b06ca756df0be0544fbdc0bc330834e55d18a5b
zxczsz <76661187+zxczsz@users.noreply.github.com>
2021-01-12 16:16:26 +0700

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -93,17 +93,18 @@ def get_readable_message():
     with download_dict_lock:
         msg = ""
         for download in list(download_dict.values()):
-            msg += f"<b>📁 File Name :</b> <code>{download.name()}</code>"
-            msg += f"\n<b>🌀 Status :</b> <i>{download.status()}</i>"
-            if download.status() != MirrorStatus.STATUS_ARCHIVING and download.status() != MirrorStatus.STATUS_EXTRACTING:
+            msg += f"<b>📁 File Name :</b> <code>{download.name()}</code>\n"
+            msg += f"<b>🌀 Status :</b> {download.status()}\n"
+            msg += "<code>Processing : </code>" if download.status() != MirrorStatus.STATUS_ARCHIVING and download.status() != MirrorStatus.STATUS_EXTRACTING:
+                msg += "\n"
                 msg += f"\n<code>{get_progress_bar_string(download)} {download.progress()}</code>" \
                        f"\n<b>🔻 Downloaded :</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}" \
                        f"\n<b>⚡️ Speed :</b> {download.speed()}, \n<b>⏳ ETA :</b> {download.eta()} "
             if download.status() == MirrorStatus.STATUS_DOWNLOADING:
                 if hasattr(download, 'is_torrent'):
-                    msg += f"\n<b>🔍 Tracker :- \n🧲 Seeders :</b> {download.aria_download().num_seeders}" \
-                        f" & <b>🧲 Peers :</b> {download.aria_download().connections}"
-                msg += f"\nGID : <code>/{BotCommands.CancelMirror} {download.gid()}</code>"
+                    msg += f"\n<b>🔍 Tracker :- 🧲 Seeders :</b> {download.aria_download().num_seeders}" \
+                        f" | <b>🧲 Peers :</b> {download.aria_download().connections}"
+                msg += f"\n<b>🚫 Cancel :</b> <code>/{BotCommands.CancelMirror} {download.gid()}</code>"
             msg += "\n\n"
         return msg
 
