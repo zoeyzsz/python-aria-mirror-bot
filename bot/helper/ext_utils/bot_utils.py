8d2b9df6c4d31133529dc01e882688be82863094
zxczsz <76661187+zxczsz@users.noreply.github.com>
2021-01-12 16:44:09 +0700

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -95,11 +95,10 @@ def get_readable_message():
         for download in list(download_dict.values()):
             msg += f"<b>📁 File Name :</b> <code>{download.name()}</code>\n"
             msg += f"<b>🌀 Status :</b> {download.status()}\n"
-            msg += "<code>Processing : </code>" if download.status() != MirrorStatus.STATUS_ARCHIVING and download.status() != MirrorStatus.STATUS_EXTRACTING:
-                msg += "\n"
-                msg += f"\n<code>{get_progress_bar_string(download)} {download.progress()}</code>" \
-                       f"\n<b>🔻 Downloaded :</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}" \
-                       f"\n<b>⚡️ Speed :</b> {download.speed()}, \n<b>⏳ ETA :</b> {download.eta()} "
+            if download.status() != MirrorStatus.STATUS_ARCHIVING and download.status() != MirrorStatus.STATUS_EXTRACTING:
+            msg += f"<code>🔥 Processing : {get_progress_bar_string(download)} {download.progress()} 🔥</code>\n" \
+                f"\n<b>🔻 Downloaded :</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}" \
+                f"\n<b>⚡️ Speed :</b> {download.speed()}, \n<b>⏳ ETA :</b> {download.eta()} "
             if download.status() == MirrorStatus.STATUS_DOWNLOADING:
                 if hasattr(download, 'is_torrent'):
                     msg += f"\n<b>🔍 Tracker :- 🧲 Seeders :</b> {download.aria_download().num_seeders}" \
