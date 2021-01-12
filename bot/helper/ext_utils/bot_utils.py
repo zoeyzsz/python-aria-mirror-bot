f9bd1b5d40af90025fd08735247d63e7e1274eed
zxczsz <76661187+zxczsz@users.noreply.github.com>
2021-01-12 14:20:12 +0700

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -14,13 +14,13 @@ URL_REGEX = r"(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+"
 
 
 class MirrorStatus:
-    STATUS_UPLOADING = "Uploading...✅"
-    STATUS_DOWNLOADING = "Downloading...📥"
-    STATUS_WAITING = "Queued...📝"
-    STATUS_FAILED = "Failed 🚫. Cleaning Trash File/Folder Download"
-    STATUS_CANCELLED = "Cancelled ❎"
-    STATUS_ARCHIVING = "Archiving...🔐"
-    STATUS_EXTRACTING = "Extracting...📂"
+    STATUS_UPLOADING = "Uploading... ✅"
+    STATUS_DOWNLOADING = "Downloading... 📥"
+    STATUS_WAITING = "Queued... 📝"
+    STATUS_FAILED = "Failed 🚫... Cleaning Trash File/Folder Download"
+    STATUS_CANCELLED = "Cancelled... ❎"
+    STATUS_ARCHIVING = "Archiving... 🔐"
+    STATUS_EXTRACTING = "Extracting... 📂"
 
 
 PROGRESS_MAX_SIZE = 100 // 8
@@ -97,14 +97,16 @@ def get_readable_message():
             msg += f"\n<b>🌀 Status :</b> <i>{download.status()}</i>"
             if download.status() != MirrorStatus.STATUS_ARCHIVING and download.status() != MirrorStatus.STATUS_EXTRACTING:
                 msg += f"\n<code>{get_progress_bar_string(download)} {download.progress()}</code>" \
-                       f"\n<b>Downloaded :</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}" \
-                       f"\n<b>Speed :</b> {download.speed()}, \n<b>ETA :</b> {download.eta()} "
+                       f"\n<b>🔻 Downloaded :</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}" \
+                       f"\n<b>⚡️ Speed :</b> {download.speed()}, \n<b>⏳ ETA :</b> {download.eta()} "
             if download.status() == MirrorStatus.STATUS_DOWNLOADING:
                 if hasattr(download, 'is_torrent'):
-                    msg += f"\n<b>Info :- 🧲 Seeders :</b> {download.aria_download().num_seeders}" \
+                    msg += f"\n<b>🔍 Tracker :- 🧲 Seeders :</b> {download.aria_download().num_seeders}" \
                         f" & <b>🧲 Peers :</b> {download.aria_download().connections}"
-                msg += f"\nGID : /{BotCommands.CancelMirror} <code>{download.gid()}</code>"
-            msg += "\n\n"
+                msg += f"\nGID :  <code>/{BotCommands.CancelMirror} {download.gid()}</code>"
+            msg += "\n\n\nf'<b>🖥️ CPU :</b> {cpuUsage}%\n ' \
+            f'<b>🎛️ RAM :</b> {memory}%\n ' \
+            f'<b>🗃️ Disk :</b> {disk}%'"
         return msg
 
 
